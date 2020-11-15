59a4b1eb0b7cdba019c8fefbcbb73c43d3fb24f0
Andrea Censi <acensi@ethz.ch>
2020-11-16 00:23:43 +0100

    fix

--- src/aido_analyze/__init__.py
+++ src/aido_analyze/__init__.py
@@ -3,7 +3,7 @@ __version__ = "6.0.11"
 from zuper_commons.logs import ZLogger
 
 logger = ZLogger(__name__)
-logger.debug(f"aido-analyze version {__version__}")
+logger.debug(f"aido-analyze version {__version__} path {__file__}")
 
 from .utils_drawing import *
 from .utils_video import *
